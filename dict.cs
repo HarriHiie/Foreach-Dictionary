b6a6660be685d29fd22235766c4e17e1d9585cd3
HarriHiie <89829583+HarriHiie@users.noreply.github.com>
2023-02-20 14:06:56 +0200

    Add files via upload

---
+++ dict.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var streets = new Dictionary<string, string>();
+
+            streets.Add("Tihase", "");
+            streets.Add("Tedre", "Tedre");
+            streets.Add("Linnu Tee", "Linnu Tee");
+            streets.Add("Siili", "");
+
+            foreach (var street in streets)
+            {
+                Console.WriteLine($"{street.Key}");
+            }
+            Console.WriteLine("--------------");
+            Console.WriteLine(streets["Tedre"]);
+            Console.WriteLine(streets["Linnu Tee"]);
+            Console.WriteLine("--------------");
+            
+
+            
+
+            
+            
+            
+            
+        }
+    }
+}
